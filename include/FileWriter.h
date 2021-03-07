ce7df97c211c8f1287e3b4e827ccf1045a2b16ab
biswaroop1547 <biswaroop08@gmail.com>
2021-03-07 12:06:32 +0530

    added to FileWriter.h

--- include/FileWriter.h
+++ include/FileWriter.h
@@ -4,6 +4,11 @@
 
 #pragma once
 
+#include "common.hpp"
+
 class FileWriter {
+public :
+    void write_ascii_file(FileData<char> f, std::string file_path);
+    void write_bin_file(FileData<uint16_t> f, std::string file_path);
 
 };
